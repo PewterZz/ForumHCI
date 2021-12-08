5ff64ccaee7322babfffde79f240d274723bbf16
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-08 17:37:05 +0700

    Create JavaScript Practice 3: Climbing Stairs.js

---
+++ JavaScript Practice 3: Climbing Stairs.js
@@ -0,0 +1,25 @@
+function mincost(cost)
+{
+
+var n = cost.length;
+let list = new Array(n);
+
+if (n == 1)
+    return cost[0];
+
+list[0] = cost[0];
+list[1] = cost[1];
+
+for (let i = 2; i < n; i++)
+{
+    list[i] = Math.min(list[i - 1],
+                     list[i - 2]) + cost[i];
+}
+
+return Math.min(list[n - 2],
+                list[n - 1]);
+}
+
+
+var a=[10, 15, 20];
+console.log(mincost(a));
