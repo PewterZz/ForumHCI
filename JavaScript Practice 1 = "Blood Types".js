16841d3c50cb349952ea92cb800334dcf085c810
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-08 18:51:57 +0700

    Update JavaScript Practice 1 = "Blood Types".js

--- JavaScript Practice 1 = "Blood Types".js
+++ JavaScript Practice 1 = "Blood Types".js
@@ -1,6 +1,6 @@
 var bloodtype1 = window.prompt('Enter your bloodtype: ')
 var bloodtype2 = window.prompt('Enter the receiving bloodtype: ')
-if (bloodtype1.charAt(0) == bloodtype2.charAt(0) && bloodtype1.charAt(-1) == bloodtype2.charAt(-1) ){
+if (bloodtype1.charAt(0) == bloodtype2.charAt(0) && bloodtype1.charAt(1) == bloodtype2.charAt(1) ){
   console.log('True, you can safely give your blood')
   alert('True, you can safely give your blood')
 }
@@ -8,7 +8,7 @@ else if(bloodtype1 == "O-"){
   console.log('True, you can safely give your blood')
   alert('True, you can safely give your blood')
 }
-else if(bloodtype1 == "O+" || bloodtype2 == "O+" && bloodtype1.charAt(-1) == "+" || bloodtype2.charAt(-1) == "+"){
+else if(bloodtype1 == "O+" || bloodtype2 == "O+" && bloodtype1.charAt(1) == "+" || bloodtype2.charAt(1) == "+"){
   console.log('True, you can safely give your blood')
   alert('True, you can safely give your blood')
 }
@@ -16,7 +16,7 @@ else if(bloodtype2 == "AB+"){
   console.log('True, you can safely give your blood')
   alert('True, you can safely give your blood')
 }
-else if(bloodtype2 =="AB-" && bloodtype1.charAt(-1) == "-"){
+else if(bloodtype2 =="AB-" && bloodtype1.charAt(2) == "-"){
   console.log('True, you can safely give your blood')
   alert('True, you can safely give your blood')
 }
