bfe67bd43814d14d2ddb70d26d75efe205e7a348
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2021-12-08 15:59:10 +0700

    Create JavaScript Practice 1 = "Blood Types".js

---
+++ JavaScript Practice 1 = "Blood Types".js
@@ -0,0 +1,26 @@
+var bloodtype1 = window.prompt('Enter your bloodtype: ')
+var bloodtype2 = window.prompt('Enter the receiving bloodtype: ')
+if (bloodtype1.charAt(0) == bloodtype2.charAt(0) && bloodtype1.charAt(-1) == bloodtype2.charAt(-1) ){
+  console.log('True, you can safely give your blood')
+  alert('True, you can safely give your blood')
+}
+else if(bloodtype1 == "O-"){
+  console.log('True, you can safely give your blood')
+  alert('True, you can safely give your blood')
+}
+else if(bloodtype1 == "O+" || bloodtype2 == "O+" && bloodtype1.charAt(-1) == "+" || bloodtype2.charAt(-1) == "+"){
+  console.log('True, you can safely give your blood')
+  alert('True, you can safely give your blood')
+}
+else if(bloodtype2 == "AB+"){
+  console.log('True, you can safely give your blood')
+  alert('True, you can safely give your blood')
+}
+else if(bloodtype2 =="AB-" && bloodtype1.charAt(-1) == "-"){
+  console.log('True, you can safely give your blood')
+  alert('True, you can safely give your blood')
+}
+else{
+  console.log('False, you cannot safely give your blood')
+  alert('False, you cannot safely give your blood')
+}
